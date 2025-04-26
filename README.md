98aa145c69668611161fe76cc2a3d31e0b36f395
Akira HIRATA <hirataakira6@gmail.com>
2025-04-26 15:06:31 +0900

    Update Descriptions

--- README.md
+++ README.md
@@ -1,26 +1,39 @@
-# WIP: Figma Finder
+![Hero Image](docs/public/hero.svg)
 
-> [!WARNING]
-> This project is a work in progress and not yet complete.
+# Figma Finder
 
 https://psephopaiktes.github.io/figma-finder/
 
-![Hero Image](design/cover.png)
+A browser extension designed to quickly find and open the Figma files you need.
+
+
+<br /><br />
+
 
 ## Get the Extension
 * [Chrome Web Store](https://chrome.google.com/webstore/detail/klifomaejfaibpkbigbpljdmelbkehbe)
 <!-- * [Add-ons for Firefox](TODO) -->
 <!-- * [Edge Add-ons](TODO) -->
 
+
+<br /><br />
+
+
 ## Development
+
 A Node.js environment is required.
+
 ```zsh
 npm i
 npm start
 ```
 
+
+<!-- <br /><br />
+
+
 ## For Firefox review team
 To review the source code and build it, please run the following command:
 ```zsh
 npm run build:firefox
-```
+``` -->
--- src/lib/view/Home.svelte
+++ src/lib/view/Home.svelte
@@ -1,85 +1,85 @@
 <script lang="ts">
-  import Layout from "@/lib/Layout.svelte";
-  import i18n from "@/lib/i18n.svelte";
-  import { loadFiles } from "@/lib/store.svelte";
-  import { store } from "@/lib/store.svelte";
-  import Input from "@/lib/ui/Home/Input.svelte";
-  import NewFAB from "@/lib/ui/Home/NewFAB.svelte";
-  import SubHeader from "@/lib/ui/Home/SubHeader.svelte";
-  import Tree from "@/lib/ui/Home/Tree.svelte";
-  import Loader from "@/lib/ui/Loader.svelte";
-  import Nav from "@/lib/ui/Nav/Index.svelte";
-  import type { File, Project } from "@/types";
-  import Fuse from "fuse.js";
-  import { onMount } from "svelte";
-  import { tick } from "svelte";
+import Layout from "@/lib/Layout.svelte";
+import i18n from "@/lib/i18n.svelte";
+import { loadFiles } from "@/lib/store.svelte";
+import { store } from "@/lib/store.svelte";
+import Input from "@/lib/ui/Home/Input.svelte";
+import NewFAB from "@/lib/ui/Home/NewFAB.svelte";
+import SubHeader from "@/lib/ui/Home/SubHeader.svelte";
+import Tree from "@/lib/ui/Home/Tree.svelte";
+import Loader from "@/lib/ui/Loader.svelte";
+import Nav from "@/lib/ui/Nav/Index.svelte";
+import type { File, Project } from "@/types";
+import Fuse from "fuse.js";
+import { onMount } from "svelte";
+import { tick } from "svelte";
 
-  let loading = $state(true);
-  let query = $state("");
-  let isTimeout = $state(false);
-  let isInputed: boolean = $derived.by(() => {
-    return query.trim().length > 0;
-  });
+let loading = $state(true);
+let query = $state("");
+let isTimeout = $state(false);
+let isInputed: boolean = $derived.by(() => {
+  return query.trim().length > 0;
+});
 
-  onMount(async () => {
-    const timeoutId = setTimeout(() => {
-      isTimeout = true;
-      loading = false;
-    }, 10000);
-
-    await loadFiles();
+onMount(async () => {
+  const timeoutId = setTimeout(() => {
+    isTimeout = true;
     loading = false;
+  }, 10000);
 
-    if (timeoutId) clearTimeout(timeoutId);
-  });
+  await loadFiles();
+  loading = false;
 
-  let filterdProjects: Record<string, Project> = $derived.by(() => {
-    if (!isInputed) {
-      return store.projects;
+  if (timeoutId) clearTimeout(timeoutId);
+});
+
+let filterdProjects: Record<string, Project> = $derived.by(() => {
+  if (!isInputed) {
+    return store.projects;
+  }
+
+  const filteredProjects: Record<string, Project> = {};
+
+  for (const [projectId, project] of Object.entries(store.projects)) {
+    const fileItems = Object.entries(project.files).map(([fileId, file]) => ({
+      id: fileId,
+      name: file.name,
+    }));
+
+    const options = {
+      keys: ["name"],
+      includeScore: true,
+      threshold: 0.2,
+      ignoreLocation: true,
+      useExtendedSearch: true,
+    };
+    const fuse = new Fuse(fileItems, options);
+    const searchResults = fuse.search(query);
+
+    const filteredFiles: Record<string, File> = {};
+    for (const result of searchResults) {
+      const fileId = result.item.id;
+      filteredFiles[fileId] = project.files[fileId];
     }
 
-    const filteredProjects: Record<string, Project> = {};
-
-    for (const [projectId, project] of Object.entries(store.projects)) {
-      const fileItems = Object.entries(project.files).map(([fileId, file]) => ({
-        id: fileId,
-        name: file.name,
-      }));
-
-      const options = {
-        keys: ["name"],
-        includeScore: true,
-        threshold: 0.2,
-        ignoreLocation: true,
-        useExtendedSearch: true,
+    if (Object.keys(filteredFiles).length > 0) {
+      filteredProjects[projectId] = {
+        ...project,
+        files: filteredFiles,
       };
-      const fuse = new Fuse(fileItems, options);
-      const searchResults = fuse.search(query);
-
-      const filteredFiles: Record<string, File> = {};
-      for (const result of searchResults) {
-        const fileId = result.item.id;
-        filteredFiles[fileId] = project.files[fileId];
-      }
-
-      if (Object.keys(filteredFiles).length > 0) {
-        filteredProjects[projectId] = {
-          ...project,
-          files: filteredFiles,
-        };
-      }
     }
+  }
 
-    return filteredProjects;
+  return filteredProjects;
+});
+
+const openFirstFile = () => {
+  tick().then(() => {
+    const firstFile = document.querySelector(".projects > li .files > li a");
+    if (!firstFile) return;
+    window.open(firstFile.getAttribute("href") || "");
   });
-
-  const openFirstFile = () => {
-    tick().then(() => {
-      const firstFile = document.querySelector(".projects > li .files > li a");
-      if (!firstFile) return;
-      window.open(firstFile.getAttribute("href") || "");
-    });
-  };
+};
 </script>
 
 <Layout>
